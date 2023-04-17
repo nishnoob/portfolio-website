d268ba660f88a0a59a9a889f058f86f68bc6c794
Agnish Mukherjee <agnish15m@gmail.com>
2023-04-17 23:03:09 +0530

    first commit

---
+++ pages/index.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function HomePage() {
+  return (
+    <>
+      <style jsx>
+        {`
+          div {
+            color: red;
+          }
+        `}
+      </style>
+      <div>Welcome to Next.js!</div>
+    </>
+  );
+}
+
+export default HomePage
